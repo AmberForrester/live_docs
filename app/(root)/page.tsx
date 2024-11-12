2e5679968fdc1ef3fbfe2cac8b2641b7d928a05c
Amber <157823790+AmberForrester@users.noreply.github.com>
2024-11-12 09:15:27 -0500

    Setup & Metadata

---
+++ app/(root)/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+
+
+const Home = () => {
+  return (
+    <div>
+      <Button>
+        Click Me
+      </Button>
+    </div>
+  )
+};
+
+export default Home;
---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Inter as FontSans } from "next/font/google";
+import { cn } from "@/lib/utils";
+import "./globals.css";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
+
+
+export const metadata: Metadata = {
+  title: "LiveDocs",
+  description: "Your real-time collaborative editor.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={cn(
+          "min-h-screen font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
