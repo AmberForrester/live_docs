1d8e9467d261a18132a5c1a269e3f0e221f9904d
Amber <157823790+AmberForrester@users.noreply.github.com>
2024-11-16 21:23:43 -0500

    Live Features with Floating Comments

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,5 +1,6 @@
 import CollaborativeRoom from "@/components/CollaborativeRoom";
 import { getDocument } from "@/lib/actions/room.actions";
+import { getClerkUsers } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
@@ -17,13 +18,25 @@ const Document = async ({ params: { id } }: SearchParamProps) => {
 
   if(!room) redirect("/");
 
-  // TODO: Assess the permission level that the user has to access the document. 
+  const userIds = Object.keys(room.usersAccesses);
+  const users = await getClerkUsers({ userIds });
+
+  const usersData = users.map((user: User) => ({
+    ...user,
+    userType: room.usersAccesses[user.email]?.includes("room:write")
+    ? "editor"
+    : "viewer"
+  }))
+
+  const currentUserType = room.users.Accesses[clerkUser.emailAddresses[0].emailAddress]?.includes("room:write") ? "editor" : "viewer";
 
   return (
     <main className="flex w-full flex-col items-center">
         <CollaborativeRoom 
           roomId={id}
           roomMetadata={room.metadata}
+          users={usersData}
+          currentUserType={currentUserType}
         />
     </main>
   )
